a1edb12fcef57f893a026361750d3b57ee3aaed5
Osvaldo Muguercia <elosvadel84@gmail.com>
2024-02-06 17:39:21 -0500

    add endpoint /agent-udate-info no authentication

--- src/features/controllers/agent.controller.js
+++ src/features/controllers/agent.controller.js
@@ -27,9 +27,34 @@ const byratingAgents = async(req, res) => {
     }
     return res.status(200).send({ message: 'List of agents sorted by rating', data: sortedAgents });
 };
+const updatingAgent = async (req, res) => {
+    const allowedKeys = ['first_name', 'last_name', 'email', 'region'];
+    const updates = Object.keys(req.body);
+    console.log(allowedKeys);                                                       // debugging purposes
+    console.log(updates);                                                           // debugging purposes
+    const isValidOperation = Object.keys(req.body).every((update) => allowedKeys.includes(update));
 
-module.exports = {
-    createAgent,
-    sortingAgents,
-    byratingAgents
+    if (!isValidOperation) {
+        return res.status(400).send({ message: 'Invalid updates!' });
+    }
+
+    try {
+        const agent = await agentSchema.findById(req.params.id); // BUT NOT ID
+
+        if (!agent) {
+            return res.status(404).send({ message: 'Agent not found' });
+        }
+
+        updates.forEach((update) => {
+            agent[update] = req.body[update];
+        });
+
+        await agent.save();
+
+        return res.status(200).send({ message: 'Agent updated successfully', data: agent });
+    } catch (error) {
+        return res.status(500).send({ message: 'Error updating agent' });
+    }
 };
+
+module.exports = { createAgent, sortingAgents, byratingAgents, updatingAgent };
--- src/routes/agent.routes.js
+++ src/routes/agent.routes.js
@@ -3,7 +3,8 @@ const {agentMiddleware} = require('../shared/middleware/base_middleware.js');
 const agentRoutes = (app) => {
     app.post('/agent-create', agentMiddleware, AgentController.createAgent);  // in the middle we need to call a middleware to validate the body
     app.get ('/agents', agentMiddleware, AgentController.sortingAgents); // in the middle we need to call a middleware to validate the body 
-    app.get ('/agents-by-region', agentMiddleware, AgentController.byratingAgents);                                                        
+    app.get ('/agents-by-region', agentMiddleware, AgentController.byratingAgents);  
+    app.patch ('/agent-update-info/:id', agentMiddleware, AgentController.updatingAgent);                                                     
 };
 
 module.exports = {
